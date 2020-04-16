942c75846f55db2764253c3ca1d991ad64f92ed0
Ernesto <laurete.student@gmail.com>
2020-04-16 17:51:06 -0500

    version P(n) con while

---
+++ Combinatoria.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int p(int n){
+    int resultado = 1;
+    int k = 0;
+    
+    k = 1;
+    while(k <= n ){
+        resultado = resultado * k;
+        k = k + 1;    
+    }
+     
+    
+    return resultado;
+}
+
+int main(){
+    printf("######################################\n");
+    printf("## LOS CALCULOS DE BETTY Y VERONICA ##\n");
+    printf("######################################\n");
+    printf("\n\n");
+    
+    printf("Betty ordena sus pulseras de %d formas    \n", p(3));
+    printf("Veronica ordena sus pulseras de %d formas \n", p(10));
+    
+    system("PAUSE");
+    return 0;   
+}
